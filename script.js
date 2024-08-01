69d37ac8199b576dd2d2c9204f36fb6abdd9c360
Mukul Singh <135323609+Mukul1598@users.noreply.github.com>
2024-08-01 17:03:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-function mincost(arr)
-{ 
+function mincost(arr) { 
 //write your code here
 // return the min cost
-  
+
+	const MinHeap = require('collections/heap');
+	
+	const minHeap = new MinHeap(arr, null, (a, b) => b - a);
+	
+	let totalCost = 0;
+	
+	while (minHeap.length > 1) {
+		const first = minHeap.pop();
+		const second = minHeap.pop();
+		const cost = first + second;
+		minHeap.push(cost);
+		totalCost += cost;
+	}
+	return totalCost;
 }
 
 module.exports=mincost;
