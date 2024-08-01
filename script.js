ec02469f85406435d2d6ed23a3f6b6f423738685
Mukul Singh <135323609+Mukul1598@users.noreply.github.com>
2024-08-01 17:16:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,20 +2,31 @@ function mincost(arr) {
 //write your code here
 // return the min cost
 
-	const MinHeap = require('collections/heap');
-	
-	const minHeap = new MinHeap(arr, null, (a, b) => b - a);
-	
-	let totalCost = 0;
-	
-	while (minHeap.length > 1) {
-		const first = minHeap.pop();
-		const second = minHeap.pop();
-		const cost = first + second;
-		minHeap.push(cost);
-		totalCost += cost;
-	}
-	return totalCost;
+const PriorityQueue = require('priorityqueuejs');
+    const minHeap = new PriorityQueue((a, b) => b - a);
+
+    // Push all rope lengths into the min-heap
+    for (let i = 0; i < arr.length; i++) {
+        minHeap.enq(arr[i]);
+    }
+
+    let totalCost = 0;
+
+    // Keep connecting ropes until we have only one rope left
+    while (minHeap.size() > 1) {
+        // Extract the two smallest lengths
+        let first = minHeap.deq();
+        let second = minHeap.deq();
+
+        // Cost to connect them
+        let cost = first + second;
+        totalCost += cost;
+
+        // Push the resulting rope back into the min-heap
+        minHeap.enq(cost);
+    }
+
+    return totalCost;
 }
 
 module.exports=mincost;
